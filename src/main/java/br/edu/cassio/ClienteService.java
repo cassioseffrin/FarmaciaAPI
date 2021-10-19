f550f9cd20e5e1a6d8a1a2da23b9043c8293c47a
Cassio Seffrin <cassioseffrin@gmail.com>
2021-10-18 22:07:01 -0300

    inicio da api

--- src/main/java/br/edu/cassio/ClienteService.java
+++ src/main/java/br/edu/cassio/ClienteService.java
@@ -7,15 +7,14 @@ import br.edu.cassio.model.Cliente;
 
 @Component
 public class ClienteService implements ClienteInterface {
-	
-	
+
 	private ClienteDao clienteDao;
 
 	public String getCliente(Integer id) {
-		
+
 		clienteDao = new ClienteDao();
 		Cliente cli = clienteDao.buscar(id);
-		
+
 		return "Ola cliente " + cli.getNome() + "!";
 	}
 }
